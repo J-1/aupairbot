ee214696f859807a2e2368460ca04c6a17b0544a
Jacob Carr <jacobcarr@Jacobs-MBP.att.net>
2015-09-05 15:27:20 -0700

    le matrix

---
+++ js/matrix.js
@@ -0,0 +1,53 @@
+var matrix_canvas = document.getElementById("matrix_canvas");
+var ctx = matrix_canvas.getContext("2d");
+
+//making the canvas full screen
+matrix_canvas.height = window.innerHeight;
+matrix_canvas.width = window.innerWidth;
+
+//chinese characters - taken from the unicode charset
+var chinese = "田由甲申甴电甶男甸甹町画甼甽甾甿畀畁畂畃畄畅畆畇畈畉畊畋界畍畎畏畐畑";
+//converting the string into an array of single characters
+chinese = chinese.split("");
+
+var font_size = 10;
+var columns = matrix_canvas.width; //number of columns for the rain
+//an array of drops - one per column
+var drops = [];
+//x below is the x coordinate
+//1 = y co-ordinate of the drop(same for every drop initially)
+for(var x = 0; x < columns; x++)
+	drops[x] = 1;
+
+var drawvar = 30;
+//drawing the characters
+function draw()
+{
+  drawvar++;
+  ctx.clearRect(0, 0, matrix_canvas.width, matrix_canvas.height);
+
+	//Black BG for the canvas
+	//translucent BG to show trail
+	ctx.fillStyle = "rgba(255, 255, 255, .01)";
+	ctx.fillRect(0, 0, matrix_canvas.width, matrix_canvas.height);
+
+	ctx.fillStyle = "rgba(0, 0, 0, .5)"; //green text
+	ctx.font = font_size + "px arial";
+	//looping over drops
+	for(var i = 0; i < drops.length; i++)
+	{
+		//a random chinese character to print
+		var text = chinese[Math.floor(Math.random()*chinese.length)];
+		//x = i*font_size, y = value of drops[i]*font_size
+		ctx.fillText(text, i*font_size, drops[i]*font_size/2);
+
+		//sending the drop back to the top randomly after it has crossed the screen
+		//adding a randomness to the reset to make the drops scattered on the Y axis
+		if(drops[i]*font_size > matrix_canvas.height && Math.random() > 0.975)
+			drops[i] = 0;
+
+		//incrementing Y coordinate
+		drops[i]++;
+	}
+}
+setInterval(draw, drawvar);
